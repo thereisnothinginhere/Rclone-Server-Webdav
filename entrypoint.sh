b0cb8d21d14b8f3687d7ffea6cc8f198e5285851
erickyun <71968711+erickyun@users.noreply.github.com>
2022-04-05 13:53:02 +0300

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#author https://github.com/erickyun/Rclone-Server-Webdav (don't delete this)
-rclone version
-curl "$CONFIG_IN_URL"> /home/rc.conf
-rclone listremotes
-rclone rcd --rc-web-gui --rc-no-auth --rc-addr=0.0.0.0:$PORT
-#rclone serve wedav $CLOUDNAME: --rc-addr=0.0.0.0:$PORT --config=/home/rc.conf --vfs-read-chunk-size 128M
