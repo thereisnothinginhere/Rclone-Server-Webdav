edbc5186319027972a66a9f4da37c756f0a67b55
erickyun <71968711+erickyun@users.noreply.github.com>
2022-04-05 13:13:10 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 rclone version
 curl "$CONFIG_IN_URL"> /home/rc.conf
 rclone listremotes
+rclone rcd --rc-web-gui --rc-no-auth --rc-addr=0.0.0.0:$PORT
 #rclone serve wedav $CLOUDNAME: --rc-addr=0.0.0.0:$PORT --config=/home/rc.conf --vfs-read-chunk-size 128M
